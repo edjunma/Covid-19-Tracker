6c4525f843cc045a930208e6ec68b2ba6bfdcd0a
edjunma <edjunma@gmail.com>
2020-06-11 03:13:09 -0400

    fetch api for chart data

--- src/api/index.js
+++ src/api/index.js
@@ -11,3 +11,17 @@ export const fetchData = async () => {
 		return { confirmed, recovered, deaths, lastUpdate };
 	} catch (error) {}
 };
+
+export const fetchDailyData = async () => {
+	try {
+		const { data } = await axios.get(`${url}/daily`);
+
+		return data.map(({ confirmed, deaths, reportDate: date }) => ({
+			confirmed: confirmed.total,
+			deaths: deaths.total,
+			date,
+		}));
+	} catch (error) {
+		return error;
+	}
+};
